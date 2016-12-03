6ef59d87c4ec2fc19e5d732ac5624b790bd11592
Kelton J. Hislop <khislop@bb136-14.adit.mines.edu>
2016-12-03 12:09:25 -0700

    first commit

---
+++ project4.h
@@ -0,0 +1,25 @@
+//
+//  project4.h
+//  Project4
+//
+//  Created by Phillip Romig on 4/3/12.
+//  Copyright 2012 Colorado School of Mines. All rights reserved.
+//
+
+#ifndef project4_h
+#define project4_h
+
+// System include files
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <iostream>
+#include <pcap/pcap.h>
+
+// Include files specific to this project.
+#include "resultsC.h"
+
+// Include files specific to this project.
+void pk_processor(u_char *, const struct pcap_pkthdr *, const u_char *);
+
+#endif
